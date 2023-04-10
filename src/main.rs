84e19b9c37e9c8d1068580b120626f966b91fce8
Tamok <nautiluce@gmail.com>
2023-04-10 01:38:44 -0700

    Set up a basic game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::time::Duration;
+use std::thread::sleep;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Game logic goes here
+
+        sleep(Duration::from_millis(100)); // Adjust the duration to control the game loop's speed
+    }
 }
