95e7f2ee728117778987179a665b74b4823829e6
Tamok <nautiluce@gmail.com>
2023-04-13 00:53:37 -0700

    Add character and quest widgets to UI

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,21 @@
 use crate::game_state::GameState;
-use crate::character::Character;
 use iced::{
-    Application, Column, Command, Container, Element, Text, button, Button, Length,
+    Application, Column, Command, Container, Element, Length, Row, Settings, Text, button, Button,
 };
 
+pub fn run() -> Result<(), iced::Error> {
+    IncrementalGame::run(Settings::default())
+}
+
 pub struct IncrementalGame {
-    game_state: GameState,
+    state: GameState,
     resource_button_state: button::State,
+}
+
+impl IncrementalGame {
+    fn gain_resource(&mut self) {
+        self.state.resources += 1;
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -14,48 +23,90 @@ pub enum Message {
     ResourceButtonPressed,
 }
 
-impl IncrementalGame {
-    pub fn new() -> Self {
-        IncrementalGame {
-            game_state: GameState::new(),
-            resource_button_state: button::State::new(),
-        }
-    }
-}
-
 impl Application for IncrementalGame {
     type Executor = iced::executor::Default;
-    type Flags = Character;
+    type Flags = ();
     type Message = Message;
 
-    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (IncrementalGame::new(), Command::none())
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (
+            IncrementalGame {
+                state: GameState::new(),
+                resource_button_state: button::State::new(),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("Incremental Unfolding Game")
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::ResourceButtonPressed => {
+                self.gain_resource();
+            }
+        }
         Command::none()
     }
 
-    fn view(&mut self) -> Element<Self::Message> {
+    fn view(&mut self) -> Element<Message> {
+        let IncrementalGame {
+            state,
+            resource_button_state,
+        } = self;
+    
+        let character = character_widget(&state.character);
+    
+        let quests = state
+            .quests
+            .iter()
+            .zip(&mut state.quest_button_states)
+            .map(|(quest, button_state)| quest_widget(quest, button_state))
+            .collect::<Column<_>>();
+    
         let content = Column::new()
-            .padding(20)
             .spacing(20)
-            .push(Text::new("Resources:"))
+            .push(character)
+            .push(quests)
             .push(
-                Button::new(&mut self.resource_button_state, Text::new("Gain Resource"))
-                    .padding(10)
-                    .on_press(Message::ResourceButtonPressed),
+                Button::new(
+                    resource_button_state,
+                    Text::new("Gain resources").size(20),
+                )
+                .on_press(Message::ResourceButtonPressed),
             );
-
+    
         Container::new(content)
             .width(Length::Fill)
             .height(Length::Fill)
             .center_x()
             .center_y()
             .into()
-        }
+    }
+    
+
+fn character_widget(character: &Character) -> Column<Message> {
+    let name = Text::new(&character.name)
+        .size(30)
+        .color([1.0, 1.0, 1.0])
+        .width(Length::Fill);
+    
+    let resources = Text::new(format!("Resources: {}", character.resources))
+        .size(20)
+        .color([1.0, 1.0, 1.0])
+        .width(Length::Fill);
+    
+    Column::new()
+        .spacing(10)
+        .push(name)
+        .push(resources)
+}
+
+fn quest_widget(quest: &Quest, button_state: &mut button::State) -> Button<Message> {
+    let label = format!("{} - Cost: {}", quest.name, quest.cost);
+    
+    Button::new(button_state, Text::new(label).size(16))
+        .on_press(Message::QuestSelected(quest.id))
 }
