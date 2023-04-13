9f97dfe4d094ee2cb69853d378e4097afd08ab81
Tamok <nautiluce@gmail.com>
2023-04-12 21:43:43 -0700

    Fix the borrow issue in character.rs

--- src/character.rs
+++ src/character.rs
@@ -1,29 +1,34 @@
 use crate::ability::{Ability, create_ability_list};
 
+#[derive(Default)]
 pub struct Character {
     pub name: String,
     pub level: u32,
+    pub experience: f64,
     pub resources: u32,
     pub abilities: Vec<Ability>,
 }
 
 impl Character {
-    pub fn execute_ability(&mut self, index: usize) {
-        if index < self.abilities.len() {
-            let ability = &self.abilities[index];
-            if self.resources >= ability.cost {
-                (ability.effect)(self);
-                self.resources -= ability.cost;
-            }
+    pub fn new(name: &str) -> Character {
+        Character {
+            name: name.to_string(),
+            level: 1,
+            experience: 0.0,
+            resources: 0,
+            abilities: create_ability_list(),
         }
     }
-}
 
-pub fn create_character(name: &str) -> Character {
-    Character {
-        name: name.to_string(),
-        level: 1,
-        resources: 0,
-        abilities: create_ability_list(),
-    }
+    pub fn execute_ability(&mut self, index: usize) {
+        if index < self.abilities.len() {
+            let cost = self.abilities[index].cost;
+            let effect = self.abilities[index].effect;
+    
+            if self.resources >= cost {
+                effect(self);
+                self.resources -= cost;
+            }
+        }
+    }    
 }
