820ba8abd5cf3be4da1ecf11a656297c092c04c5
Tamok <nautiluce@gmail.com>
2023-04-15 01:54:14 -0700

    Update ability.rs to use data from JSON files

--- src/ability.rs
+++ src/ability.rs
@@ -12,18 +12,8 @@ pub struct Ability {
     pub cost: u32,
 }
 
-pub fn create_ability_list() -> Vec<Ability> {
-    vec![
-        Ability {
-            name: "Gather".to_string(),
-            description: "Gather resources from the environment.".to_string(),
-            cost: 1,
-            effect: gather_effect,
-        },
-        // Add more abilities here as needed.
-    ]
-}
-
-fn gather_effect(character: &mut Character) {
-    character.resources += 1;
+impl Ability {
+    pub fn gather_effect(&self, character: &mut Character) {
+        character.resources += 1;
+    }
 }
