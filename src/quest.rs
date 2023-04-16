99893b5c091b4a820ade516b4b201d25ffeb4a28
Tamok <nautiluce@gmail.com>
2023-04-16 01:06:19 -0700

    Update quest.rs to remove example_data method and rely on quests.json

--- src/quest.rs
+++ src/quest.rs
@@ -12,40 +12,12 @@ pub struct Quest {
 }
 
 impl Quest {
-    pub fn new(name: &str, description: &str) -> Self {
+    pub fn new(name: &str, description: &str, id: u32, reward: u32) -> Self {
         Self {
             name: name.to_string(),
             description: description.to_string(),
-            is_active: false,
-            is_complete: false,
-            id: 1,
-            cost: 10
+            id,
+            reward,
         }
-    }
-
-    pub fn activate(&mut self) {
-        self.is_active = true;
-    }
-
-    pub fn complete(&mut self) {
-        self.is_active = false;
-        self.is_complete = true;
-    }
-
-    pub fn example_data() -> Vec<Quest> {
-        vec![
-            Quest {
-                id: 1,
-                name: String::from("First Quest"),
-                description: String::from("This is the first quest."),
-                cost: 10,
-            },
-            Quest {
-                id: 2,
-                name: String::from("Second Quest"),
-                description: String::from("This is the second quest."),
-                cost: 20,
-            },
-        ]
     }
 }
